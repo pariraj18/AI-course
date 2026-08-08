9e4ba3437a5af1371724b33596ce3618a8f3bf5b
pariraj18 <pariraj18092006@gmail.com>
2026-08-08 20:26:31 +0530

    Add AI course Week 1

---
+++ Week_1/Day_2/sys_temp.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key =os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("GROQ_API_KEY environment variable is not set.")
+
+client = Groq(api_key=my_api_key)
+model="llama-3.3-70b-versatile"
+role="user"
+prompt="Suggest a name for my cloth company."
+message_system={"role": "system",
+            "content": "You are a brand manager who suggests name for my company.name should be in one word"}
+message = {"role": role,
+            "content": prompt}
+messages = [message_system, message]
+# Temperature is a parameter that controls the randomness of the model's output. A higher temperature (e.g., 2) will result in more random and creative responses, while a lower temperature (e.g., 0.2) will produce more focused and deterministic responses.
+response = client.chat.completions.create(model=model, messages=messages, temperature=0)
+#print(response)
+
+print("###################")
+answer=response.choices[0].message.content
+print(answer)
